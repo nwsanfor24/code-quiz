4e89f95666aa0b25f575afcbd4dbcd2716bfd706
Nathan Sanford <nwsanfor24@gmail.com>
2020-01-22 10:21:31 -0500

    Add placeholder questions to external JS file

---
+++ script.js
@@ -0,0 +1,42 @@
+const codingQuestions = [
+    {
+        question: "Who is the fairest of them all?",
+        answers: {
+            a: "Snow White",
+            b: "Sleeping Beauty",
+            c: "Waluigi, obviously",
+            d: "Wario, but it's probably not"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who is Luke's Father?",
+        answers: {
+            a: "Papa Palpatine",
+            b: "Darth Vader",
+            c: "Ben Kenobi",
+            d: "Rey Skywalker"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Why is my Dad such a dick?",
+        answers: {
+            a: "Because he's fat",
+            b: "Because he's right",
+            c: "Because when you look in the mirror, you see his face and not yours so you start questioning your reality and wondering if this is your future",
+            d: "It's definitely C but we'll throw this one in for good measure"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Placeholder question",
+        answers: {
+            a: "Answer",
+            b: "Answer",
+            c: "Answer",
+            d: "The Right Answers"
+        },
+        correctAnswer: "d"
+    },
+];
